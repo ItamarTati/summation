7924389a6ad1a662a4067ede71bfb9db374d000d
ItamarTati <itamar.tati1994@gmail.com>
2023-10-14 00:01:31 +0100

    completed

---
+++ src/Main.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("Hello user welcome to my summation app");
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter first number");
+
+        int firstNumber = scanner.nextInt();
+
+        System.out.println("Enter second number");
+
+        int secondNumber = scanner.nextInt();
+
+        int total = firstNumber + secondNumber;
+        System.out.println("Your total value is " + total);
+
+    }
+}
